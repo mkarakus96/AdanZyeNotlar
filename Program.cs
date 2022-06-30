0b58c7ebc29ebc8eb4752a9ed548f3720bf8680e
mkarakus96 <muhammedkarakus96@gmail.com>
2022-06-30 16:30:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region tuple Örneği
+            (int a, string b) c = (5, "deneme");
+            Console.WriteLine(c.a);
+            #endregion
+        }
+    }
+}
