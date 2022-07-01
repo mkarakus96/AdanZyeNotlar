bb742cea8e50e2a99c80de704e9ee77396c4d06e
mkarakus96 <muhammedkarakus96@gmail.com>
2022-07-01 11:08:25 +0300

    Ternary

--- Program.cs
+++ Program.cs
@@ -6,25 +6,35 @@ namespace ProjectExample
     {
         static void Main(string[] args)
         {
-            #region (_a, _b) =(a, b) Tuple Türüyle Değer Atama
-            (int a, string b) c = (5, "deneme");
-            Console.WriteLine(c.a);
+
+            #region Ternary Operatoru
+            bool medeniHal = false;
+            string evli = "evli";
+            string bekar = "bekar";
+            string mesaj;
+            mesaj = medeniHal ? evli : bekar;
+            Console.WriteLine(mesaj);
             #endregion
 
-            #region Değişken Türüne Uygun Default Değer Atama
-            bool x = default(bool);//değişkene default değerini atamak için kullanılır.
-            int y = default(int);
-            string z = default(string);
-            char d = default(char);
+            //#region (_a, _b) =(a, b) Tuple Türüyle Değer Atama
+            //(int a, string b) c = (5, "deneme");
+            //Console.WriteLine(c.a);
+            //#endregion
 
-            //main içerisinde oluşturulan değişkenlerin ilk değerlerini manuel atmaya özen göstermeliyiz.
+            //#region Değişken Türüne Uygun Default Değer Atama
+            //bool x = default(bool);//değişkene default değerini atamak için kullanılır.
+            //int y = default(int);
+            //string z = default(string);
+            //char d = default(char);
 
-            #endregion
-            #region Default Literals Default değer atama(C# 7.1)
-            bool e = default;
-            decimal u = default;
-            //7.1'den sonra bu şekilde yazmamz yeterli 
-            #endregion
+            ////main içerisinde oluşturulan değişkenlerin ilk değerlerini manuel atmaya özen göstermeliyiz.
+
+            //#endregion
+            //#region Default Literals Default değer atama(C# 7.1)
+            //bool e = default;
+            //decimal u = default;
+            ////7.1'den sonra bu şekilde yazmamz yeterli 
+            //#endregion
 
         }
     }
